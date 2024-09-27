29a08d6162d0c76a4b24dcf2cc325ca6161dee8f
cactusbento <cactusbento@outlook.com>
2024-09-27 11:00:47 -0500

    init

---
+++ lua/plugins/main.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    opts = {
+      setup = {
+        transparent = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+  },
+}
